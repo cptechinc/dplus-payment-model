629a1889af4b6de7176ce1b6581d5259e0fe545f
Paul Gomez <pauldro952@hotmail.com>
2020-11-02 16:28:35 -0600

    Payment Decryption

--- src/Model/Payment.php
+++ src/Model/Payment.php
@@ -6,14 +6,7 @@ use Dplus\Payments\Model\ThrowErrorTrait;
 use Dplus\Payments\Model\MagicMethodTraits;
 
 /**
- * Skeleton subclass for representing a row from the 'Payment' table.
- *
- *
- *
- * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
- * long as it does not already exist in the output directory.
- *
+ * Class for representing a row from the 'Payment' table.
  */
 class Payment extends BasePayment {
 	use ThrowErrorTrait;
@@ -46,122 +39,195 @@ class Payment extends BasePayment {
 		'ordn'              => 'ordernbr',
 		'recordtype'        => 'rectype',
 		'cardnumber'        => 'cardnbr',
-		'track1'            => 'track_i',
-		'track2'            => 'track_ii',
+		'track1'            => 'tracki',
+		'track2'            => 'trackii',
 		'expiredate'        => 'expdate',
-		'transaction_type'  => 'type',
-		'transaction_id'    => 'trans_id',
-		'authorizationcode' => 'authcode'
+		'transaction_type'  => 'rectype',
+		'transaction_id'    => 'transid',
+		'transactionid'     => 'transid',
+		'authorizationcode' => 'authcode',
+		'avs'               => 'avsmsg'
 	);
 
+	/**
+	 * Set Salt Value for Encryption
+	 * @param   string $salt
+	 * @return void
+	 */
 	public function set_salt($salt) {
 		$this->salt = $salt;
 	}
 
 	/**
-	 * Returns if Payment Record is a Request
-	 *
-	 * @return boolean
+	 * Set Salt Value for Encryption
+	 * @param   string $salt
+	 * @return void
+	 */
+	public function get_salt() {
+		return $this->salt;
+	}
+
+	/**
+	 * Return if Payment Record is a Request
+	 * @return bool
 	 */
 	public function is_request() {
 		return $this->rectype == self::RECORDTYPE_REQUEST;
 	}
 
 	/**
-	 * Returns if Payment Record is a Response
-	 *
-	 * @return boolean
+	 * Return if Payment Record is a Response
+	 * @return bool
 	 */
 	public function is_response() {
 		return $this->rectype == self::RECORDTYPE_RESPONSE;
 	}
 
 	/**
-	 * Returns Decrypted Card Number
-	 * 
-	 * @return string 
+	 * Return if Card Track Data is present
+	 * @return bool
 	 */
-	public function cardnumber() {
-		$q = PaymentQuery::create();
-		$q->select_decrypted_cardnumber($this->salt);
-		$q->filterByOrdn($this->ordn);
-		$q->filterByRecordtype($this->recordtype);
-		$q->findOne();
+	public function is_card_present() {
+		return strlen($this->track2()) || strlen($this->trackii);
 	}
 
 	/**
-	 * Returns Decrypted Card Expiration Date
-	 * 
-	 * @return string 
+	 * Return if Card Track Data is NOT present
+	 * @return bool
 	 */
-	public function expiredate() {
-		$q = PaymentQuery::create();
-		$q->select_decrypted_expiredate($this->salt);
-		$q->filterByOrdn($this->ordn);
-		$q->filterByRecordtype($this->recordtype);
-		$q->findOne();
+	public function is_cnp() {
+		return (strlen($this->track2()) || strlen($this->trackii)) === false;
 	}
 
 	/**
-	 * Returns Decrypted Card CVV
-	 * 
-	 * @return string 
+	 * Update Date / Time
+	 * @return void
 	 */
-	public function cvv() {
-		$q = PaymentQuery::create();
-		$q->select_decrypted_cvv($this->salt);
-		$q->filterByOrdn($this->ordn);
-		$q->filterByRecordtype($this->recordtype);
-		$q->findOne();
-	}
-
-	/**
-	 * Returns Decrypted Card Track1 Data
-	 * 
-	 * @return string 
-	 */
-	public function track1() {
-		$q = PaymentQuery::create();
-		$q->select_decrypted_track1($this->salt);
-		$q->filterByOrdn($this->ordn);
-		$q->filterByRecordtype($this->recordtype);
-		$q->findOne();
-	}
-
-	/**
-	 * Returns Decrypted Card Track2 Data
-	 * 
-	 * @return string 
-	 */
-	public function track2() {
-		$q = PaymentQuery::create();
-		$q->select_decrypted_track2($this->salt);
-		$q->filterByOrdn($this->ordn);
-		$q->filterByRecordtype($this->recordtype);
-		$q->findOne();
+	public function set_datetime() {
+		$this->setDate(date('Ymd'));
+		$this->setTime(date('His'));
 	}
 
 	/**
 	 * Sets Result property to Approved
-	 *
 	 * @return void
 	 */
-	public function set_result_approved() {
+	public function approve() {
 		$this->result = self::RESULT_APPROVED;
 	}
 
 	/**
 	 * Sets Result property to Declined
-	 *
 	 * @return void
 	 */
-	public function set_result_declined() {
+	public function decline() {
 		$this->result = self::RESULT_DECLINED;
 	}
 
+/* =============================================================
+	Decrypt Column Functions
+============================================================= */
+	/**
+	 * Return Decrypted Column Value
+	 * @param  string $col  Database Column
+	 * @return mixed        Value
+	 */
+	public function decrypt($col) {
+		$q = PaymentQuery::create();
+		$q->select_decrypted_column($col, $this->salt);
+		$q->filterByOrdn($this->ordn);
+		$q->filterByRecordtype($this->recordtype);
+		return $q->findOne();
+	}
+
+	/**
+	 * Returns Decrypted Card Number
+	 * @return string 
+	 */
+	public function cardnumber() {
+		return $this->decrypt('cardnbr');
+	}
+
+	/**
+	 * Returns Decrypted Card Expiration Date
+	 * @return string 
+	 */
+	public function expiredate() {
+		return $this->decrypt('expdate');
+	}
+
+	/**
+	 * Returns Decrypted Card CVV
+	 * @return string 
+	 */
+	public function cvv() {
+		return $this->decrypt('cvv');
+	}
+
+	/**
+	 * Returns Decrypted Card Track1 Data
+	 * @return string 
+	 */
+	public function track1() {
+		return $this->decrypt('track_i');
+	}
+
+	/**
+	 * Returns Decrypted Card Track2 Data
+	 * @return string 
+	 */
+	public function track2() {
+		return $this->decrypt('track_ii');
+	}
+	
+/* =============================================================
+	Encrypt Functions
+============================================================= */
+	/**
+	 * Encrypts Card Number in the Database
+	 *  @return void
+	 */
+	public function encrypt_cardnumber() {
+		PaymentQuery::create()->encrypt_column('cardnbr', $this->salt, $this->ordn);
+	}
+
+	/**
+	 * Encrypts Expiration Date in the Database
+	 * @return void
+	 */
+	public function encrypt_expiredate() {
+		PaymentQuery::create()->encrypt_column('expdate', $this->salt, $this->ordn);
+	}
+
+	/**
+	 * Encrypts CVV in the Database
+	 * @return void
+	 */
+	public function encrypt_cvv() {
+		PaymentQuery::create()->encrypt_column('cvv', $this->salt, $this->ordn);
+	}
+
+	/** 
+	 * Encrypts Track1 data in the Database
+	 * @return void
+	 */
+	public function encrypt_track1() {
+		PaymentQuery::create()->encrypt_column('track_i', $this->salt, $this->ordn);
+	}
+
+	/**
+	 * Encrypts Track2 data in the Database
+	 * @return void
+	 */
+	public function encrypt_track2() {
+		PaymentQuery::create()->encrypt_column('track_ii', $this->salt, $this->ordn);
+	}
+
+/* =============================================================
+	Instance Functions
+============================================================= */
 	/**
 	 * Creates a Payment object with the Rectype of Response
-	 *
 	 * @return Payment
 	 */
 	public static function create_response() {
@@ -179,48 +245,5 @@ class Payment extends BasePayment {
 		$request = new self();
 		$request->setRectype(self::RECORDTYPE_REQUEST);
 		return $request;
-	}
-
-	/**
-	 * Encrypts Card Number in the Database
-	 *  @return void
-	 */
-	public function encrypt_cardnumber() {
-		PaymentQuery::create()->encrypt_request_expiredate($this->salt, $this->ordn);
-	}
-
-	/**
-	 * Encrypts Expiration Date in the Database
-	 * @return void
-	 */
-	public function encrypt_expiredate($salt, $ordn) {
-		PaymentQuery::create()->encrypt_request_expiredate($this->salt, $this->ordn);
-	}
-
-
-
-	/**
-	 * Encrypts CVV in the Database
-	 * @return void
-	 */
-	public function encrypt_cvv() {
-		PaymentQuery::create()->encrypt_request_cvv($this->salt, $this->ordn);
-	}
-
-	/** 
-	 * Encrypts Track1 data in the Database
-	 * @return void
-	 */
-	public function encrypt_track1() {
-		PaymentQuery::create()->encrypt_request_track1($this->salt, $this->ordn);
-	}
-
-
-	/**
-	 * Encrypts Track2 data in the Database
-	 * @return void
-	 */
-	public function encrypt_track2() {
-		PaymentQuery::create()->encrypt_request_track2($this->salt, $this->ordn);
 	}
 }
--- src/Model/PaymentQuery.php
+++ src/Model/PaymentQuery.php
@@ -5,134 +5,34 @@ use Base\PaymentQuery as BasePaymentQuery;
 use Dplus\Payments\Model\QueryTraits;
 
 /**
- * Skeleton subclass for performing query and update operations on the 'Payment' table.
- *
- *
- *
- * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
- * long as it does not already exist in the output directory.
- *
+ * Class for performing query and update operations on the 'Payment' table.
  */
 class PaymentQuery extends BasePaymentQuery {
 	use QueryTraits;
 
 	/**
-	 * Selects Decrypted Card Number
+	 * Add Decrypted Column to Query
 	 *
-	 * @param string $salt Value to Decrypt with
-	 * @return void
+	 * @param string $col   Column
+	 * @param string $salt  Salt Value
+	 * @return PaymentQuery 
 	 */
-	public function select_decrypted_cardnumber($salt) {
-		$this->withColumn("cast(aes_decrypt(cardnbr, hex($salt)) as char charset utf8", 'cardnumber');
-		$this->select('cardnumber');
+	public function select_decrypted_column($col, $salt) {
+		$this->withColumn("cast(aes_decrypt($col, hex('$salt')) as char charset utf8)", 'decrypt');
+		$this->select('decrypt');
+		return $this;
 	}
 
 	/**
-	 * Encrypts Card Number in the Database
-	 *
-	 * @param string $salt Value to Encrypt with
-	 * @param string $ordn Sales Order number
+	 * Executes Query to Encrypt Column
+	 * @param  string $col   Column
+	 * @param  string $salt  Salt Value
+	 * @param  string $ordn  Order Number
 	 * @return void
 	 */
-	public function encrypt_request_cardnumber($salt, $ordn) {
-		$sql = "UPDATE authnet SET cardnbr = AES_ENCRYPT(cardnbr, HEX('$salt') WHERE ordernbr = :ordn AND rectype = 'REQ'";
-		$params = array(':ordn' => $ordn);
-		$this->execute_query($sql, $params);
-	}
-
-	/**
-	 * Selects Decrypted Card Expiration Date
-	 *
-	 * @param string $salt Value to Decrypt with
-	 * @return void
-	 */
-	public function select_decrypted_expiredate($salt) {
-		$this->withColumn("cast(aes_decrypt(expdate, hex($salt)) as char charset utf8", 'expiredate');
-		$this->select('expiredate');
-	}
-
-	/**
-	 * Encrypts Expiration Date in the Database
-	 *
-	 * @param string $salt Value to Encrypt with
-	 * @param string $ordn Sales Order number
-	 * @return void
-	 */
-	public function encrypt_request_expiredate($salt, $ordn) {
-		$sql = "UPDATE authnet SET expdate = AES_ENCRYPT(expdate, HEX('$salt') WHERE ordernbr = :ordn AND rectype = 'REQ'";
-		$params = array(':ordn' => $ordn);
-		$this->execute_query($sql, $params);
-	}
-
-	/**
-	 * Selects Decrypted Card CVV
-	 *
-	 * @param string $salt Value to Decrypt with
-	 * @return void
-	 */
-	public function select_decrypted_cvv($salt) {
-		$this->withColumn("cast(aes_decrypt(cvv, hex($salt)) as char charset utf8", 'cvv');
-		$this->select('cvv');
-	}
-
-	/**
-	 * Encrypts CVV in the Database
-	 *
-	 * @param string $salt Value to Encrypt with
-	 * @param string $ordn Sales Order number
-	 * @return void
-	 */
-	public function encrypt_request_cvv($salt, $ordn) {
-		$sql = "UPDATE authnet SET cvv = AES_ENCRYPT(cvv, HEX('$salt') WHERE ordernbr = :ordn AND rectype = 'REQ'";
-		$params = array(':ordn' => $ordn);
-		$this->execute_query($sql, $params);
-	}
-
-	/**
-	 * Selects Decrypted Card Track1 Data
-	 *
-	 * @param string $salt Value to Decrypt with
-	 * @return void
-	 */
-	public function select_decrypted_track1($salt) {
-		$this->withColumn("cast(aes_decrypt(track_i, hex($salt)) as char charset utf8", 'track1');
-		$this->select('track1');
-	}
-
-	/**
-	 * Encrypts Track1 data in the Database
-	 *
-	 * @param string $salt Value to Encrypt with
-	 * @param string $ordn Sales Order number
-	 * @return void
-	 */
-	public function encrypt_request_track1($salt, $ordn) {
-		$sql = "UPDATE authnet SET track_i = AES_ENCRYPT(track_i, HEX('$salt') WHERE ordernbr = :ordn AND rectype = 'REQ'";
-		$params = array(':ordn' => $ordn);
-		$this->execute_query($sql, $params);
-	}
-
-	/**
-	 * Selects Decrypted Card Track2 Data
-	 *
-	 * @param string $salt Value to Decrypt with
-	 * @return void
-	 */
-	public function select_decrypted_track2($salt) {
-		$this->withColumn("cast(aes_decrypt(track_ii, hex($salt)) as char charset utf8", 'track2');
-		$this->select('track2');
-	}
-
-	/**
-	 * Encrypts Track2 data in the Database
-	 *
-	 * @param string $salt Value to Encrypt with
-	 * @param string $ordn Sales Order number
-	 * @return void
-	 */
-	public function encrypt_request_track2($salt, $ordn) {
-		$sql = "UPDATE authnet SET track_ii = AES_ENCRYPT(track_ii, HEX('$salt') WHERE ordernbr = :ordn AND rectype = 'REQ'";
+	public function encrypt_column($col, $salt, $ordn) {
+		$sql = "UPDATE authnet SET cardnbr = AES_ENCRYPT($col, HEX('$salt')) WHERE ordernbr = :ordn AND rectype = 'REQ'";
+		echo $sql;
 		$params = array(':ordn' => $ordn);
 		$this->execute_query($sql, $params);
 	}
